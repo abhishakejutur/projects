9171a1bdcac529e1bfd15a979e55bbd6227bf76e
Abhishake Jutur <91953148+abhishakejutur@users.noreply.github.com>
2022-12-28 20:48:45 +0530

    Update age_and_gender_detection.py
    Age and Gender detector that can approximately guess the gender and age of the person (face) in a picture using the deep learning...

--- Image Processing Projects/Age and Gender Detection/age_and_gender_detection.py
+++ Image Processing Projects/Age and Gender Detection/age_and_gender_detection.py
@@ -74,3 +74,4 @@ while cv2.waitKey(1)<0:
         cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
         cv2.imshow("Detecting age and gender", resultImg)
 cv2.destroyAllWindows()
+
